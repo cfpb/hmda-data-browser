c9cfdec849c2e491f85cc3a36f0c361d64695159
Patrick Asata <patrick.asata@cfpb.gov>
2019-05-08 11:52:09 -0400

    updating list of races

--- src/constants/races.js
+++ src/constants/races.js
@@ -1,21 +1,11 @@
 export default [
   { id: '1', name: 'American Indian or Alaska Native' },
   { id: '2', name: 'Asian' },
-  { id: '21', name: 'Asian Indian' },
-  { id: '22', name: 'Chinese' },
-  { id: '23', name: 'Filipino' },
-  { id: '24', name: 'Japanese' },
-  { id: '25', name: 'Korean' },
-  { id: '26', name: 'Vietnamese' },
-  { id: '27', name: 'Other Asian' },
   { id: '3', name: 'Black Or African American' },
   { id: '4', name: 'Native Hawaiian Or Other Pacific Islander' },
-  { id: '41', name: 'Native Hawaiian' },
-  { id: '42', name: 'Guamanian Or Chamorro' },
-  { id: '43', name: 'Samoan' },
-  { id: '44', name: 'Other Pacific Islander' },
   { id: '5', name: 'White' },
-  { id: '6', name: 'Race Information Not Provided' },
-  { id: '7', name: 'Race Not Applicable' },
-  { id: '8', name: 'Race No CoApplicant' }
+  { id: '6', name: '2 or more minority races' },
+  { id: '7', name: 'Joint' },
+  { id: '8', name: 'Free Form Text Only' },
+  { id: '9', name: 'Race Not Available' }
 ]
