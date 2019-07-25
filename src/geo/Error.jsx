a356a22facafe8cf6c09f8855c47856a1bbef842
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-07-25 09:36:25 -0700

    fix error message typo

--- src/geo/Error.jsx
+++ src/geo/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Alert from '../common/Alert'
 
 const Error = ({error}) => {
-  const standard = <p>Sorry, we were unable to complete your request. We have been notified of the issue are and working to resolve it.</p>
+  const standard = <p>Sorry, we were unable to complete your request. We have been notified of the issue and are working to resolve it.</p>
   const rateLimited = <p>Sorry, we are currently serving a high volume of requests. Please try again later.</p>
   return (
     <div className="Error">
